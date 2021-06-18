a9453201ffcca9cbe4864fd20ea764d8a143d30e
Richard Muthwill <richardmuthwill@hotmail.com>
2021-06-18 13:31:40 +0200

    Added "CompassDirection" method to "Snippets.cs"

---
+++ Snippets.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Snippets : MonoBehaviour
+{
+    // Turn a vector into a compass
+    string CompassDirection(Vector2 v)
+    {
+        string direction = "";
+
+        v.Normalize();
+
+        if (Vector3.Angle(v, Vector3.forward) <= 45.0)
+        {
+            direction = "up";
+        }
+        else if (Vector3.Angle(v, Vector3.right) <= 45.0)
+        {
+            direction = "right";
+        }
+        else if (Vector3.Angle(v, Vector3.back) <= 45.0)
+        {
+            direction = "down";
+        }
+        else
+        {
+            direction = "left";
+        }
+
+        return direction;
+    }
+}
