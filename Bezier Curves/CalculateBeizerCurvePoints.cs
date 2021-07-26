4105fd3caa8ad21e012e87efda9bc84d18f1edd0
Richard Muthwill <richardmuthwill@hotmail.com>
2021-07-26 10:57:36 +0200

    Added Bezier Curves

---
+++ Bezier Curves/CalculateBeizerCurvePoints.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CalculateBeizerCurvePoints : MonoBehaviour
+{
+    [SerializeField] Transform start, end, p1, p2, startOut, endOut;
+
+    QuadBez curve;
+    CubicBez curve2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // curve = new QuadBez(start.position, end.position, p1.position);
+        curve2 = new CubicBez(start.position, end.position, p1.position, p2.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newP1 = new Vector3(start.position.x + (end.position.x - start.position.x) / 3, start.position.y + (end.position.y - start.position.y) / 3, start.position.z + (end.position.z - start.position.z) / 3);
+        // Vector3 newP2 = new Vector3(end.position.x + (start.position.x - end.position.x) / 3, end.position.y + (start.position.y - end.position.y) / 3, end.position.z + (start.position.z - end.position.z) / 3);
+
+        p1.position = Vector3.Lerp(p1.position, startOut.position, 15 * Time.deltaTime);
+        p2.position = Vector3.Lerp(p2.position, endOut.position, 3 * Time.deltaTime);
+
+        // curve = new QuadBez(start.position, end.position, p1.position);
+        curve2 = new CubicBez(start.position, end.position, p1.position, p2.position);
+    }
+    
+	void OnDrawGizmos()
+	{
+        Vector3 newP1 = new Vector3(start.position.x + (end.position.x - start.position.x) / 3, start.position.y + (end.position.y - start.position.y) / 3, start.position.z + (end.position.z - start.position.z) / 3);
+
+        p1.position = Vector3.Lerp(p1.position, startOut.position, 15 * Time.deltaTime);
+        p2.position = Vector3.Lerp(p2.position, endOut.position, 3 * Time.deltaTime);
+
+        curve2 = new CubicBez(start.position, end.position, p1.position, p2.position);
+        curve2.GizmoDraw(1f);
+    }
+}
---
+++ Bezier Curves/DrawBezierCurve.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(LineRenderer))]
+public class DrawBezierCurve : MonoBehaviour
+{
+    public Transform[] controlPoints;
+    public LineRenderer lineRenderer;
+    public Transform startCurve, endCurve;
+
+    private int curveCount = 0;
+    private int layerOrder = 0;
+    private int SEGMENT_COUNT = 50;
+
+    [SerializeField] private int startSmooth = 15;
+    [SerializeField] private int endSmooth = 10;
+
+    void Start()
+    {
+        if (!lineRenderer)
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+        lineRenderer.sortingLayerID = layerOrder;
+        curveCount = (int)controlPoints.Length / 3;
+    }
+
+    void Update()
+    {
+        controlPoints[0 + 1].position = Vector3.Lerp(controlPoints[0 + 1].position, startCurve.position, startSmooth * Time.deltaTime);
+        controlPoints[(controlPoints.Length - 1) - 1].position = Vector3.Lerp(controlPoints[(controlPoints.Length - 1) - 1].position, endCurve.position, endSmooth * Time.deltaTime);
+
+        DrawCurve();
+    }
+
+    void DrawCurve()
+    {
+        for (int j = 0; j < curveCount; j++)
+        {
+            for (int i = 1; i <= SEGMENT_COUNT; i++)
+            {
+                float t = i / (float)SEGMENT_COUNT;
+                int nodeIndex = j * 3;
+                Vector3 pixel = CalculateCubicBezierPoint(t, controlPoints[nodeIndex].position, controlPoints[nodeIndex + 1].position, controlPoints[nodeIndex + 2].position, controlPoints[nodeIndex + 3].position);
+                lineRenderer.positionCount = ((j * SEGMENT_COUNT) + i);
+                lineRenderer.SetPosition((j * SEGMENT_COUNT) + (i - 1), pixel);
+            }
+        }
+    }
+
+    Vector3 CalculateCubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+
+        Vector3 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
+
+        return p;
+    }
+}
