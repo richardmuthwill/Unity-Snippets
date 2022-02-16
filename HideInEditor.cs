dab0b7ff22c7ac42bfe3663bafe062638e9ac8e5
Richard Muthwill <richardmuthwill@hotmail.com>
2022-02-16 10:49:07 +0100

    Added HideInEditor.cs

---
+++ HideInEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class Autorun
+{
+    static Autorun()
+    {
+        EditorApplication.update += Update;
+    }
+
+    static void Update()
+    {
+        SceneVisibilityManager.instance.Hide(GameObject.FindGameObjectsWithTag("HideInEditor"), true);
+    }
+}
